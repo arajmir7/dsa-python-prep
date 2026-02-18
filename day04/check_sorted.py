0c1ddbbb07a37c8f0f81537897d29e382e32964a
Ajmir Aribam <arajmir7@gmail.com>
2026-02-18 19:30:15 +0530

    Check Sorted element in the Array

---
+++ day04/check_sorted.py
@@ -0,0 +1,10 @@
+def isSorted(arr, n):
+    for i in range(1, n):
+        if arr[i] < arr[i - 1]:
+            return False
+    return True 
+
+arr = [1, 2, 3, 4, 5]
+n = len(arr)
+
+print("True" if isSorted(arr, n) else "False")
