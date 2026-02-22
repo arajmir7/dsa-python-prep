7f6eec779e5d2906478a237c37a8b0509413cfcf
Ajmir Aribam <arajmir7@gmail.com>
2026-02-22 22:21:18 +0530

    Selection Sort in Array

---
+++ day08/selection_sort.py
@@ -0,0 +1,17 @@
+class Solution:
+    def selectionSort(self, arr):
+        n = len(arr)
+
+        for i in range(n):
+            min_index = i
+
+            for j in range(i + 1, n):
+                if arr[j] < arr[min_index]:
+                    min_index = j
+
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+
+        return arr
+
+arr = [13, 46, 24, 52, 20, 9]
+print("Sorted array:", Solution().selectionSort(arr))
