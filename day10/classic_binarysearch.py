67731e4408dc01f5e280e7749618d53e36f1cd4f
Ajmir Aribam <arajmir7@gmail.com>
2026-02-25 19:02:30 +0530

    Classic Binary Search

---
+++ day10/classic_binarysearch.py
@@ -0,0 +1,18 @@
+class Solution:
+    def binarySearch(self, arr, target):
+        low, high = 0, len(arr) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return -1
+
+arr = [1, 3, 5, 7, 9]
+print(Solution().binarySearch(arr, 7))
