a3b3620b275d06be968108b32a59c114201b57d7
Ajmir Aribam <ajmiraribam@Ajmirs-MacBook-Air.local>
2026-03-08 22:35:22 +0530

    “Max_Depth”

---
+++ day22/max_depth.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+
+sol = Solution()
+print("Max Depth:", sol.maxDepth(root))
