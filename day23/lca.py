42aa8092fd91b69682bf1efc3a527dd601f04c25
Ajmir Aribam <ajmiraribam@Ajmirs-MacBook-Air.local>
2026-03-09 22:26:43 +0530

    “Lowest_Common_Anchestor”

---
+++ day23/lca.py
@@ -0,0 +1,34 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if not root or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+
+        return left if left else right
+
+
+root = TreeNode(3)
+root.left = TreeNode(5)
+root.right = TreeNode(1)
+root.left.left = TreeNode(6)
+root.left.right = TreeNode(2)
+root.left.right.left = TreeNode(7)
+root.left.right.right = TreeNode(4)
+
+p = root.left.left   
+q = root.left.right  
+
+lca = Solution().lowestCommonAncestor(root, p, q)
+print("LCA:", lca.val)
