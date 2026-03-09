3a856a2eb88d14a14afe6d1886035fefcfe879fd
Ajmir Aribam <ajmiraribam@Ajmirs-MacBook-Air.local>
2026-03-09 22:22:57 +0530

    “Right_Side_View”

---
+++ day23/right_side_view.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def rightSideView(self, root):
+        if not root:
+            return []
+
+        queue = deque([root])
+        result = []
+
+        while queue:
+            size = len(queue)
+
+            for i in range(size):
+                node = queue.popleft()
+
+                if i == size - 1:
+                    result.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return result
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.right = TreeNode(5)
+root.right.right = TreeNode(4)
+
+print("Right Side View:", Solution().rightSideView(root))
