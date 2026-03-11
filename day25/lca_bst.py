8b64948cc9dc29140d61491f9946ed6773bddc40
Ajmir Aribam <ajmiraribam@Ajmirs-MacBook-Air.local>
2026-03-11 22:09:15 +0530

    “LCA_in_BST”

---
+++ day25/lca_bst.py
@@ -0,0 +1,31 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+
+        while root:
+            if p.val < root.val and q.val < root.val:
+                root = root.left
+            elif p.val > root.val and q.val > root.val:
+                root = root.right
+            else:
+                return root
+
+root = TreeNode(6)
+root.left = TreeNode(2)
+root.right = TreeNode(8)
+root.left.left = TreeNode(0)
+root.left.right = TreeNode(4)
+root.right.left = TreeNode(7)
+root.right.right = TreeNode(9)
+
+p = root.left       
+q = root.right      
+
+lca = Solution().lowestCommonAncestor(root, p, q)
+
+print("LCA:", lca.val)
